a31d4e9f912a8cc186b1f665c761b1d34166ced6
derkrisz <krisztian.der@gmail.com>
2018-02-09 07:01:42 +0000

    extend seeds until week 9

--- back_end/seeds.js
+++ back_end/seeds.js
@@ -107,3 +107,84 @@ db.syllabus.insert({
     content: ["Projects!"]
     }
 });
+
+db.syllabus.insert({
+  week_number: 6,
+  week_title: "Java",
+  language: "Java",
+  days: [
+    {
+    number: 1,
+    content: ["Intro to Java/IDEs", "Single classes & encapsulation", "Types", "ArrayLists", "Loops & Conditionals", "Eightball lab"]
+    },
+    {
+    number: 2,
+    content: ["Multiple classes", "Buses lab", "Hashmaps", "Enums", "Quiz"]
+    },
+    {
+    number: 3,
+    content: ["All day lab", "Generics lesson"]
+    },
+    {
+    number: 4,
+    content: ["Generics lab", "Inheritance", "Packages & access modifiers", "Try/Catch", "Employee lab"]
+    },
+    {
+    number: 5,
+    content: ["Weekend homework"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 7,
+  week_title: "Java",
+  language: "Java",
+  days: [
+    {
+    number: 1,
+    content: ["Abstract Classes", "Interfaces", "Stereo system lab"]
+    },
+    {
+    number: 2,
+    content: ["Polymorphism", "Polymorphism lab", "Composition", "Composition lab"]
+    },
+    {
+    number: 3,
+    content: ["Adventure time lab"]
+    },
+    {
+    number: 4,
+    content: ["SOLID review", "SOLID lab", "Intro to Android", "Robot Android exercise"]
+    },
+    {
+    number: 5,
+    content: ["Weekend homework - Music shop"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 8,
+  week_title: "Android",
+  language: "Java + Android",
+  days: [
+    {
+    number: 1,
+    content: ["Basic android app", "Addig controls", "Deployment", "Word counter lab"]
+    },
+    {
+    number: 2,
+    content: ["Activities and intents", "Custom Listview Adapters"]
+    },
+    {
+    number: 3,
+    content: ["Rock paper scissors lab"]
+    },
+    {
+    number: 4,
+    content: ["Data persistence", "Menu", "Movies ranking lab"]
+    },
+    {
+    number: 5,
+    content: ["Project time!"]
+    }
+});
