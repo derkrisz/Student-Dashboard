f04739f11084c14e13349bab8113a07e3dcb771f
Sigi5mund <ajwright124@gmail.com>
2018-02-09 10:22:05 +0000

    Backend Server up and running

---
+++ back_end/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+
+server.use(bodyParser.json());
+server.use(express.static('client/build'));
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(cors());
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("codeclan_dashboard");
+
+
+  server.get('/api/syllabus', function(req, res){
+    db.collection('syllabus').find().toArray(function(err, results){
+      if (err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.json(results);
+      console.log("Sillybus successs");
+    })
+  });
+
+
+
+
+  server.listen(5000, function() {
+    console.log('Listening on port 5000');
+  });
+
+
+});
