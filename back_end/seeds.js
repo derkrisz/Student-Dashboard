de2dd0daf66b21994f0ed76d3d1e3b59961e13a0
annalanigan <annalanigan@outlook.com>
2018-02-09 08:54:40 +0000

    added to syllabus seeds

--- back_end/seeds.js
+++ back_end/seeds.js
@@ -169,7 +169,7 @@ db.syllabus.insert({
   days: [
     {
     number: 1,
-    content: ["Basic android app", "Addig controls", "Deployment", "Word counter lab"]
+    content: ["Basic android app", "Adding controls", "Deployment", "Word counter lab"]
     },
     {
     number: 2,
@@ -186,5 +186,194 @@ db.syllabus.insert({
     {
     number: 5,
     content: ["Project time!"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 9,
+  week_title: "Java/A ndroidProject",
+  language: "Project Week",
+  days: [
+    {
+    number: 1,
+    content: ["Project"]
+    },
+    {
+    number: 2,
+    content: ["Project"]
+    },
+    {
+    number: 3,
+    content: ["Project"]
+    },
+    {
+    number: 4,
+    content: ["PRESENTATIONS"]
+    },
+    {
+    number: 5,
+    content: ["Review"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 10,
+  week_title: "CS",
+  language: "CS and CV",
+  days: [
+    {
+    number: 1,
+    content: ["Algorithms", "CV/PDA", "IT Industry Landscape", "CV/BIO workshop"]
+    },
+    {
+    number: 2,
+    content: ["Data Structures", "Brand of ME"]
+    },
+    {
+    number: 3,
+    content: ["TEchnical Testing and CV's", "Parallel Programming"]
+    },
+    {
+    number: 4,
+    content: ["Interview Skills Workshop"]
+    },
+    {
+    number: 5,
+    content: ["PDA Test", "CV Reviews", "Mock Interviews"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 11,
+  week_title: "JS Foundations",
+  language: "JavaScript",
+  days: [
+     {
+     number: 1,
+     content: ["JS Intro - Fundamentals", "Functions, Scope, Let & Const"]
+     },
+     {
+     number: 2,
+     content: ["Objects and Constructor Functions", "Prototypal Inheritance", "Water Bottle TDD". "OO Bank"]
+     },
+     {
+     number: 3,
+     content: ["Intro to callbacks", "Enumeration"]
+     },
+     {
+     number: 4,
+     content: ["This", "Lodash", "Closures"]
+     },
+     {
+     number: 5,
+     content: ["NPM recap"]
+     }
+ });
+
+db.syllabus.insert({
+  week_number: 12,
+  week_title: "JS in the Browser",
+  language: "JavaScript",
+  days: [
+    {
+    number: 1,
+    content: ["Browser JS intro", "DOM review", "Reading the DOM", "Writing the DOM"]
+    },
+    {
+    number: 2,
+    content: ["Event Listeners", "Local storage", "Todo app Lab". "Google Maps intro"s]
+    },
+    {
+    number: 3,
+    content: ["JS Requests", "Git Branching", "Countries pair lab"]
+    },
+    {
+    number: 4,
+    content: ["Canvas", "Charts"]
+    },
+    {
+    number: 5,
+    content: ["PDA lesson"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 13,
+  week_title: "JS Full-stack",
+  language: "JavaScript",
+  days: [
+    {
+    number: 1,
+    content: ["Intro to Express", "Express REST", "Webpack"]
+    },
+    {
+    number: 2,
+    content: ["Intro to Mongo", "Serve up API from DB"]
+    },
+    {
+    number: 3,
+    content: ["Event Loop Video", "Practical GitHub", "Bucket List all day Lab"]
+    },
+    {
+    number: 4,
+    content: ["What is Scrum?", "Hand out Group Projects"]
+    },
+    {
+    number: 5,
+    content: ["Group Projects"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 14,
+  week_title: "Group Project",
+  language: "Group Project",
+  days: [
+    {
+    number: 1,
+    content: ["Group Project"]
+    },
+    {
+    number: 2,
+    content: ["Group Project"]
+    },
+    {
+    number: 3,
+    content: ["Group Project"]
+    },
+    {
+    number: 4,
+    content: ["PRESENTATIONS"]
+    },
+    {
+    number: 5,
+    content: ["Speed Networking"]
+    }
+});
+
+db.syllabus.insert({
+  week_number: 15,
+  week_title: "React",
+  language: "React",
+  days: [
+    {
+    number: 1,
+    content: ["ES6 intro", "Frameworks Introduction", "React Intro, React Hello World, React Piggy Bank", "React Comments"]
+    },
+    {
+    number: 2,
+    content: ["Add a form to React comments", "React Lifecycle", "React countries", "API pair lab"]
+    },
+    {
+    number: 3,
+    content: ["Quiz", "Tic Tac Toe all day lab"]
+    },
+    {
+    number: 4,
+    content: ["React Router", "Socket.io", "Socket lab"]
+    },
+    {
+    number: 5,
+    content: ["Mock Interviews"]
     }
 });
