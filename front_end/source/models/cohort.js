45c5527143c1faf4536a83e74d847fac8d06dd6e
derkrisz <krisztian.der@gmail.com>
2018-02-11 20:35:41 +0000

    refactor/cohort week no is assigned to const. week

--- front_end/source/models/cohort.js
+++ front_end/source/models/cohort.js
@@ -1,20 +1,20 @@
 const Request = require('../services/request');
 
 
-const Cohort = function(name, start_date, teachers, week){
+const Cohort = function(name, start_date, teachers){
   this.name = name;
   this.start_date = start_date;
   this.teachers = teachers;
   this.syllabus= "";
-  this.week = week;
+  this.week = 0;
 }
 
-Cohort.prototype.getNoOfWeeks = function(){
+Cohort.prototype.setNoOfWeeks = function(){
   var oneWeek = 7*24*60*60*1000; // hours*minutes*seconds*milliseconds
   var startDate = this.start_date;
   var currentDate = new Date(); // can you use todays date here?
   var diffWeeks = Math.round(Math.abs((startDate.getTime() - currentDate.getTime())/(oneWeek)));
-  return diffWeeks-2;
+  this.week += diffWeeks-1;
 }
 
 module.exports = Cohort;
--- front_end/source/models/specs/cohort_spec.js
+++ front_end/source/models/specs/cohort_spec.js
@@ -4,19 +4,18 @@ var assert = require('assert');
 describe ('Cohort', function () {
   var cohort1;
   var cohort2;
-  var cohort3;
   var teachers;
 });
 
 beforeEach(function () {
   teachers = ["Keith", "Sandy", "Sian", "Upul"];
-  cohort1 = new Cohort("E17", new Date(2017, 09, 30), teachers, 13);
-  cohort2 = new Cohort("E18", new Date(2017,10,20), 10);
-  cohort3 = new Cohort("E19", new Date(2018,00,25), 2);
+  cohort1 = new Cohort("E17", new Date(2017, 09, 30), teachers);
+  cohort2 = new Cohort("E18", new Date(2017,10,20));
 });
 
 it('should count the week numbers since starting', function() {
-  assert.equal(cohort1.getNoOfWeeks(), 13)
-  assert.equal(cohort2.getNoOfWeeks(), 10)
-  assert.equal(cohort3.getNoOfWeeks(), 1)
+  cohort1.setNoOfWeeks();
+  cohort2.setNoOfWeeks();
+  assert.equal(cohort1.week, 14)
+  assert.equal(cohort2.week, 11)
 });
