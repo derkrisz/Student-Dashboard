083a4b0fb2eba6c2aad5159e953523f711f7ea1e
annalanigan <annalanigan@outlook.com>
2018-02-11 18:04:43 +0000

    sorted the nav bar and added a suggestion into cohort.js

--- front_end/source/models/cohort.js
+++ front_end/source/models/cohort.js
@@ -9,4 +9,15 @@ const Cohort = function(name, start_date, teachers, week){
   this.week = week;
 }
 
+// not currently taking the 2 week holiday into account?
+const getNoOfWeeks = function(){
+  var oneWeek = 7*24*60*60*1000; // hours*minutes*seconds*milliseconds
+  var firstDate = new Date(2018,01,08);
+  var secondDate = new Date(2018,02,12); // can you use todays date here?
+  var diffWeeks = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneWeek)));
+  return diffWeeks;
+}
+
+console.log(getNoOfWeeks());
+
 module.exports = Cohort;
