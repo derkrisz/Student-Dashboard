42f9ded0e1d04919613df0ec79638b13996a7e94
annalanigan <annalanigan@outlook.com>
2018-02-12 08:41:05 +0000

    merging

---
+++ front_end/source/views/date_view.js
@@ -0,0 +1,16 @@
+const DateView = function() {
+
+}
+
+DateView.prototype.displayDate = function () {
+  const now = new Date();
+  dateField = document.querySelector('#date');
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  dateField.innerText = now.toLocaleDateString('gb-GB', options);
+};
+
+DateView.prototype.dynamicDate = function () {
+  setInterval(this.displayDate, 1000);
+};
+
+module.exports = DateView;
