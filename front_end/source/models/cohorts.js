5f211fc4e1630e31bc0a73eb643bf22b3bf39a02
derkrisz <krisztian.der@gmail.com>
2018-02-12 10:52:14 +0000

    refactor current cohort week to incorporate chrimbo break

--- front_end/source/models/cohorts.js
+++ front_end/source/models/cohorts.js
@@ -9,9 +9,9 @@ var teachers1 = ["Keith", "Sandy", "Sian", "Upul"];
 var teachers2 =["Craig", "Zsolt", "John"];
 var teachers3 = ["Darren", "Finn", "Jarrod"];
 
-cohort1 = new Cohort("E17", new Date(2017, 09, 30), teachers1);
-cohort2 = new Cohort("E18", new Date(2017, 10, 20), teachers2);
-cohort3 = new Cohort("E19", new Date(2018, 0, 29), teachers3);
+cohort1 = new Cohort("E17", new Date(2017, 09, 30), teachers1, true);
+cohort2 = new Cohort("E18", new Date(2017, 10, 20), teachers2, true);
+cohort3 = new Cohort("E19", new Date(2018, 0, 29), teachers3, false);
 
 cohorts.push(cohort1);
 cohorts.push(cohort2);
--- front_end/source/models/specs/cohort_spec.js
+++ front_end/source/models/specs/cohort_spec.js
@@ -5,14 +5,18 @@ describe ('Cohort', function () {
   var cohort1;
   var cohort2;
   var cohort3;
-  var teachers;
+  var teachers1;
+  var teachers2;
+  var teachers3;
 });
 
 beforeEach(function () {
-  teachers = ["Keith", "Sandy", "Sian", "Upul"];
-  cohort1 = new Cohort("E17", new Date(2017, 09, 30), teachers);
-  cohort2 = new Cohort("E18", new Date(2017, 10, 20));
-  cohort3 = new Cohort("E19", new Date(2018, 0, 29));
+  teachers1 = ["Keith", "Sandy", "Sian", "Upul"];
+  teachers2 =["Craig", "Zsolt", "John"];
+  teachers3 = ["Darren", "Finn", "Jarrod"];
+  cohort1 = new Cohort("E17", new Date(2017, 09, 30), teachers1, true);
+  cohort2 = new Cohort("E18", new Date(2017, 10, 20), teachers2, true);
+  cohort3 = new Cohort("E19", new Date(2018, 0, 29), teachers3, false);
 });
 
 it('should count the week numbers since starting', function() {
