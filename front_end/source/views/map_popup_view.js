aeaeb0b0eb6c219cd4893ffb30f3e0c0f189bffb
derkrisz <krisztian.der@gmail.com>
2018-02-14 14:51:59 +0000

    refactor mapdirections

--- front_end/source/views/map_popup_view.js
+++ front_end/source/views/map_popup_view.js
@@ -14,40 +14,37 @@ MapPopUp.prototype.populateView = function () {
   var codeclan1 = mainMap.addMarker(codeclan);
   mainMap.addInfoBubble(pub1, "The Chanter");
   mainMap.addInfoBubble(codeclan1, "CodeClan");
-  // mainMap.route({origin: pub, destination: codeclan, travelMode: 'WALKING' })
 };
 
 MapPopUp.prototype.calculateRoute = function() {
-  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    suppressMarkers: true
+  });
   var directionsService = new google.maps.DirectionsService();
 
   var mapDiv = document.querySelector('#mapPopUpMain');
   var center = { lat: 55.946613, lng: -3.203017 };
-  var codeclan = { lat: 55.946962, lng: -3.201958 };
+  var codeclan = { lat: 55.947149, lng: -3.201958 };
   var pub = { lat: 55.9458, lng: -3.2036 };
-  mainMap = new google.maps.Map(mapDiv, {
-    center: center,
-    zoom: 17
-  });
-  // var pub1 = mainMap.addMarker(pub);
-  // var codeclan1 = mainMap.addMarker(codeclan);
-  // mainMap.addInfoBubble(pub1, "The Chanter");
-  // mainMap.addInfoBubble(codeclan1, "CodeClan");
+  var mainMap = new MapWrapper(mapDiv, center, 17);
+  var pub1 = mainMap.addMarker(pub);
+  var codeclan1 = mainMap.addMarker(codeclan);
+  mainMap.addInfoBubble(pub1, "The Chanter");
+  mainMap.addInfoBubble(codeclan1, "CodeClan");
 
-  directionsDisplay.setMap(mainMap);
-  console.log(mainMap);
+  directionsDisplay.setMap(mainMap.googleMap);
 
-  var whatnot = {
+  var journey = {
     origin: codeclan,
     destination: pub,
     travelMode: 'WALKING'
   };
 
-  directionsService.route(whatnot, function(result, status) {
+  directionsService.route(journey, function(result, status) {
     if (status === "OK") {
       directionsDisplay.setDirections(result);
     }
-  })
-}
+  });
+};
 
 module.exports = MapPopUp;
